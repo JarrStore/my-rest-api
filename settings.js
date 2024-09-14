cdb87c6a2cb5df3f469b1e0fa12224dfc913b365
JarrStore <163243032+JarrStore@users.noreply.github.com>
2024-09-15 00:21:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'fajar'// yourname
+global.MONGO_DB_URI = "database mongodb" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
-global.limitCount = 10000
+global.your_email = "fajarstore709@gmail.com" //email
+global.email_password = "Jarr2233" //application password email
+global.limitCount = 9999999
 global.YUOR_PORT = 8000
 global.loghandler = {
 	noapikey:{
